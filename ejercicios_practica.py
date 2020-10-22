2af88ca46903d4cfa9271e71fd3fca9a8f431cc7
zekalarcon <zekalarcon@gmail.com>
2020-10-22 17:52:57 -0300

    Ejercicios condicionales completados

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -11,9 +11,8 @@ Programa creado para poner a prueba los conocimientos
 adquiridos durante la clase
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
-__version__ = "1.1"
+__author__ = "Ezequiel Alarcon"
+__email__ = "zekalarcon@gmail.com"
 
 
 def ej1():
@@ -29,15 +28,49 @@ def ej1():
     # Compare cual de los dos números es mayor
     # Imprima en pantalla según corresponda
 
+    if numero_1 > numero_2:
+        print(f'El primer numero ingresado es mayor que el segundo {numero_1} > {numero_2}')
+    else:
+        print(f'El segundo numero ingresado es mayor que el primero {numero_2} > {numero_1}')
+
+
     # Verifique si el numero_1 positivo, negativo o cero
     # Imprima el resultado en cada caso
+
+    if numero_1 > 0:
+        print(f'El {numero_1} es positivo')
+    elif numero_1 < 0:
+        print(f'El {numero_1} es negativo')
+    else:
+        print('El numero es 0')        
 
     # Verifique si el numero_1 es mayor a 0 y menor a 100
     # Imprima en pantalla si se cumple o no la condición
 
+    if numero_1 > 0 and numero_1 < 100:
+        print(f'El numero {numero_1} es mayor a 0 y menor que 100, cumple con la condicion')
+    else:
+        if numero_1 == 0:
+            print('El numero es 0, no cumple la condicion')
+        elif numero_1 < 0:
+            print('El numero es menor a 0, no cumple la condicion')  
+        else:
+            print('El numero es mayor a 100, no cumple la condicion')          
+
     # Verifique si el numero_1 es menor a 10 o el numero_2
     # es mayor a -2
     # Imprima en pantalla si se cumple o no la condición
+
+    if numero_1 < 10 or numero_2 > -2:
+        if numero_1 < 10:
+            print(f'El primer numero ingreado: {numero_1}\nCumple la condicion')
+        elif numero_2 > -2:
+            print(f'El segundo numero ingresado: {numero_2}\nCumple la condicion')    
+    else:
+        print('Los numeros no cumplen ninguna condicion')
+                
+          
+
 
 
 def ej2():
@@ -52,30 +85,60 @@ def ej2():
 
     # Compare cual de las dos palabras es mayor (alfabéticamente)
     # Imprima en pantalla según corresponda
+    
+    if texto_1 > texto_2:
+        print(f'La palabra "{texto_1}" es mayor alfabeticamente que la palabra "{texto_2}"')
+    elif texto_2 > texto_1:
+        print(f'La palabra "{texto_2}" es mayor alfabeticamente que la palabra "{texto_1}"')
+    else:
+        print(f'La palabra "{texto_1}" y "{texto_2}" son iguales alfabeticamente')        
 
     # Compare cual de las dos palabras tiene mayor
     # cantidad de letras
     # Imprima en pantalla según corresponda
 
+    if len(texto_1) > len(texto_2):
+        print(f'La palabra "{texto_1}" tiene mayor catidad de letras que la palabra "{texto_2}"')
+    elif len(texto_2) > len(texto_1):
+        print(f'La palabra "{texto_2}" tiene mayor catidad de letras que la palabra "{texto_1}"') 
+    else:
+        print(f'Las palabras "{texto_1}" y "{texto_2}" tienen la misma cantidad de letras')       
+
     # Verifique si la primera letra de la primera palabra
     # es mayor a la primera letra de la segunda palabra
     # Imprima en pantalla según corresponda
+
+    if texto_1[:1] > texto_2[:1]:
+        print(f'La primer letra "{texto_1[:1]}" es mayor alfabeticamente que la primer letra "{texto_2[:1]}"')
+    elif texto_2[:1] > texto_1[:1]:
+        print(f'La primer letra "{texto_2[:1]}" es mayor alfabeticamente que la primer letra "{texto_1[:1]}"')
+    else:
+        print(f'La primer letra "{texto_1[:1]}" y primer letra "{texto_2[:1]}" son iguales alfabeticamente')       
 
     copia_texto_1 = texto_1  # Copia de la variable texto_1
 
     # Verifique que copia_texto_1 es igual a texto_1
     # Imprima en pantalla según corresponda
+    if copia_texto_1 == texto_1:
+        print(f'La palabra "{copia_texto_1}" es igual a "{texto_1}"')
+    else:
+        print(f'La palabra "{copia_texto_1}" es diferente a "{texto_1}"')    
 
     # Verifique que copia_texto_1 es distinta a texto_2
     # Imprima en pantalla según corresponda
+
+    if copia_texto_1 != texto_2:
+        print(f'La palabra "{copia_texto_1}" es diferente a "{texto_2}"')
+    else:
+        print(f'La palabra "{copia_texto_1}" es igual a "{texto_2}"')    
 
 
 def ej3():
     # Ejercicios de práctica numérica
 
     # Condicionales anidados
-    numero_1 = 7
-    numero_2 = -2
+    numero_1 = 3
+    numero_2 = 5
 
     # Verifique si el numero_1 es mayor a 5
     #   --> En caso afirmativo, verifique si el numero_2
@@ -87,6 +150,17 @@ def ej3():
     #       --> En caso afirmativo imprima en pantalla "Resp=3"
     #       --> En caso negativo imprima en pantalla "Resp=4"
 
+    if numero_1 > 5:
+        if numero_2 > 0:
+            print('Resp = 1')
+        else:
+            print('Resp = 2')
+    elif numero_1 <= 5:
+        if numero_2 > 5:
+            print('Resp = 3')
+        else:
+            print('Resp = 4')            
+    
     # Verifique la calificación de un estudiante según su
     # puntaje en un examen
     puntaje = 70
@@ -100,20 +174,50 @@ def ej3():
     # Debe imprimir en pantalla la calificacion
     # Utilizar "if" anidados
 
+    if puntaje >= 90:
+        print('A')
+    elif puntaje >= 80:
+        print('B')
+    elif puntaje >= 70:
+        print('C')
+    elif puntaje >= 60:
+        print('D')
+    elif puntaje < 60:
+        print('F')
+                                 
+
 
 def ej4():
     # Ejemplos variables de texto
 
-    texto_1 = '5'
-    texto_2 = '7'
+    texto_1 = '7'
+    texto_2 = '5'
 
     # Verifique cual cual de los dos textos es mayor alfabéticamente
     # Imprima en pantalla según corresponda
+
+    if texto_1 > texto_2:
+        print(f"Alfabeticamente el string '{texto_1}' es mayor que el string '{texto_2}'")
+    elif texto_2 > texto_1:
+        print(f"Alfabeticamente el string '{texto_2}' es mayor que el string '{texto_1}'")
+    else:
+        print(f"Alfabeticamente los strings '{texto_1}' y '{texto_2}' son iguales")    
 
     # Transforma esas variables tipo texto y almacénalas
     # en nuevas variables númericas (int)
     # Repita el proceso, ¿Cuál de las nuevas variables es mayor?
     # Imprima en pantalla según corresponda
+
+    numero_1 = int(texto_1)
+    numero_2 = int(texto_2) 
+        
+           
+    if numero_1 > numero_2:
+        print(f'El numero {numero_1} es mayor que el numero {numero_2}')
+    elif numero_2 > numero_1:
+        print(f'El numero {numero_2} es mayor que el numero {numero_1}')
+    else:
+        print(f'Los numeros {numero_1} y {numero_2} son iguales')        
 
     # Para pensar!
     # ¿Por qué cree que texto_2 es mayor a texto_1?
--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -10,9 +10,9 @@ Descripcion:
 Programa creado para que practiquen los conocimietos adquiridos durante la semana
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
-__version__ = "1.3"
+__author__ = "Ezequiel Alarcon"
+__email__ = "zekalarcon@gmail.com"
+
 
 
 def ej1():
@@ -23,6 +23,20 @@ def ej1():
     Calcule la diferencia entre ellos e informe por pantalla
     si el resultado es positivo, negativo o cero.
     '''
+
+    print('Ingrese 2 numeros para saber si la diferencia es positiva, negativa, o 0')
+
+    num_1 = int(input('Ingrese primer numero:\n'))
+    num_2 = int(input('Ingrese segundo numero:\n'))
+
+    resta = num_1 - num_2
+
+    if resta > 0:
+        print(f'La diferencia entre los numeros ingresados es {resta}, numero positivo')
+    elif resta < 0:
+        print(f'La diferencia entre los numeros ingresados es {resta}, numero negativo')
+    else:
+        print('La diferencia entre los numeros ingresados es 0')        
 
 
 def ej2():
@@ -35,6 +49,27 @@ def ej2():
     Para cada caso imprimir el resultado en pantalla.
     '''
 
+    print('Ingrese 3 numeros para saber si son par o impar')
+
+    num_1 = int(input('Ingrese primer numero:\n'))
+    num_2 = int(input('Ingrese segundo numero:\n'))
+    num_3 = int(input('Ingrese tercer numero:\n'))
+
+    if (num_1 % 2) == 0:
+        print(f'Numero {num_1} es par')
+    else:
+        print(f'Numero {num_1} es impar')  
+
+    if (num_2 % 2) == 0:
+        print(f'Numero {num_2} es par')
+    else:
+        print(f'Numero {num_2} es impar') 
+
+    if (num_3 % 2) == 0:
+        print(f'Numero {num_3} es par')
+    else:
+        print(f'Numero {num_3} es impar')           
+        
 
 def ej3():
     print('Ejercicios de práctica con números')
@@ -53,6 +88,39 @@ def ej3():
     Imprimir en pantalla la operación realizada y el resultado
     '''
 
+    print('Ingrese dos numeros para realizar las siguientes operaciones:')
+
+    num_1 = int(input('Ingrese primer numero:\n'))
+    num_2 = int(input('Ingrese segundo numero:\n'))
+    
+    print(
+        '-Ingrese el simbolo (+) para suma\n'
+        '-Ingrese el simbolo (-) para resta\n'
+        '-Ingrese el simbolo (*) para multiplicacion\n'
+        '-Ingrese el simbolo (/) para division\n'
+        '-Ingrese el simbolo (**) para exponente/potencia\n'
+    )
+
+    simbolo = str(input('Ingrese simbolo:\n'))
+
+    if simbolo == '+':
+        suma = num_1 + num_2
+        print(f'La suma entre {num_1} y {num_2} es: {suma}')
+    elif simbolo == '-':
+        resta = num_1 - num_2
+        print(f'La resta entre {num_1} y {num_2} es: {resta}')
+    elif simbolo == '*':
+        multiplicacion = num_1 * num_2
+        print(f'La multiplicacion entre {num_1} y {num_2} es: {multiplicacion}')
+    elif simbolo == '/':
+        division = num_1 / num_2
+        print(f'La division entre {num_1} y {num_2} es: {division}')
+    elif simbolo == '**':
+        potencia = num_1 ** num_2
+        print(f'La potencia entre {num_1} y {num_2} es: {potencia}')
+    else:
+        print('El simbolo ingresado es incorrecto')                
+
 
 def ej4():
     print('Ejercicios de práctica con cadenas')
@@ -70,7 +138,35 @@ def ej4():
     e imprimir en pantalla de la mayor a la menor
     '''
 
+    print('Ingrese 3 palabras y elija una opcion para ver el resultado:\n')
 
+    palabra_1 = str(input('Ingrese primer palabra:\n'))
+    palabra_2 = str(input('Ingrese segunda palabra:\n'))
+    palabra_3 = str(input('Ingrese tercer palabra:\n'))
+    
+    print(
+        '- Ingrese 1 para ordenar alfabeticamente\n'
+        '- Ingrese 2 para ordenar por cantidad de letras\n'
+    )
+
+
+    opcion = int(input('Elija una opcion:\n'))
+
+    lista_palabras = (palabra_1, palabra_2, palabra_3)
+
+    if opcion == 1:
+        palabras_ordenadas = sorted(lista_palabras)
+        palabras_ordenadas = ', '.join(palabras_ordenadas)
+        print(f'Palabras ordenadas alfabeticamente:\n{palabras_ordenadas}')
+    elif opcion == 2:
+        palabras_ordenadas = sorted(lista_palabras, key=len, reverse=True)
+        palabras_ordenadas = ', '.join(palabras_ordenadas)
+        print(f'Palabras ordenadas por cantidad de caracteres:\n{palabras_ordenadas}')
+    else:
+        ('La opcion ingresada no es valida')    
+        
+
+        
 def ej5():
     print('Ejercicios de práctica con números')
 
@@ -84,9 +180,39 @@ def ej5():
     En cada caso imprimir en pantalla el resultado
     '''
 
+    print('Ingrese 3 temperaturas y elija una opcion para ver el resultado')
+
+    temp_1 = float(input('Ingrese primer temperatura:\n'))
+    temp_2 = float(input('Ingrese segunda temperatura:\n'))
+    temp_3 = float(input('Ingrese tercer temperatura:\n'))
+
+    print(
+        '- Elija 1 para saber cual temperatura es la maxima\n'
+        '- Elija 2 para saber cual temperatura es la minima\n'
+        '- Elija 3 para saber el promedio entre las tres temperaturas\n'
+    )
+
+
+    opcion = int(input('Ingrese opcion:\n'))
+
+    temperaturas = (temp_1, temp_2, temp_3)
+
+    if opcion == 1:
+        temp_max = max(temperaturas)
+        print(f'La temperatura maxima ingresada es:\n{temp_max}')
+    elif opcion == 2:
+        temp_min = min(temperaturas)  
+        print(f'La temperatura minima ingresada es:\n{temp_min}')
+    elif opcion == 3:
+        promedio = (temp_1 + temp_2 + temp_3) / 3
+        print(f'El promedio entre las temperaturas es:\n{promedio}')
+    else:
+        print('La opcion ingresada no es valida')          
+
+
 if __name__ == '__main__':
     print("Ejercicios de práctica")
-    ej1()
+    #ej1()
     #ej2()
     #ej3()
     #ej4()
